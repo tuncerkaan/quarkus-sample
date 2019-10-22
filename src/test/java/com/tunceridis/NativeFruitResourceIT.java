d1edbc31a21fc7bf395b79ff138ad9c6859f5b43
Fatih Kaan Tuncer <fatihkaantuncer@gmail.com>
2019-10-22 10:51:03 +0200

    Initial commit for sample

---
+++ src/test/java/com/tunceridis/NativeFruitResourceIT.java
@@ -0,0 +1,9 @@
+package com.tunceridis;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeFruitResourceIT extends FruitResourceTest {
+
+    // Execute the same tests but in native mode.
+}
