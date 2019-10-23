778aa9482f125485c4ac234138ce7d219331fd09
Fatih Kaan Tuncer <fatihkaantuncer@gmail.com>
2019-10-23 10:22:30 +0200

    Logging mechanism added

--- src/main/java/com/tunceridis/FruitResource.java
+++ src/main/java/com/tunceridis/FruitResource.java
@@ -5,12 +5,17 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.jboss.logging.Logger;
+
 @Path("/fruit")
 public class FruitResource {
+
+    Logger logger = Logger.getLogger(FruitResource.class);
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        logger.debug("hello method triggered");
         return "apple";
     }
 }
