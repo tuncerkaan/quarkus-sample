9a07461751bf840eb2334c0bb2f37d8bac1533d1
Fatih Kaan Tuncer <fatihkaantuncer@gmail.com>
2019-10-27 23:42:24 +0300

    property read

--- src/main/java/com/tunceridis/FruitResource.java
+++ src/main/java/com/tunceridis/FruitResource.java
@@ -5,6 +5,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
 
 @Path("/fruit")
@@ -12,10 +13,13 @@ public class FruitResource {
 
     Logger logger = Logger.getLogger(FruitResource.class);
 
+    @ConfigProperty(name = "greetings.message")
+    String message;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         logger.debug("hello method triggered");
-        return "apple";
+        return message;
     }
 }
